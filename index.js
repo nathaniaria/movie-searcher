79a3811e3a0e24d0cc90218c75a03e8241eb7eba
nathaniaria <nathan.iaria7@gmail.com>
2022-12-14 18:18:41 +1100

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+// api https://www.omdbapi.com/?i=tt3896198&apikey=f97dee03
+// 
+
+const moviesListELem = document.querySelector(".movies")
+
+
+async function main() {
+    const movies = await fetch("https://www.omdbapi.com/?i=tt3896198&apikey=f97dee03&s=fast")
+    const moviesData = await movies.json()
+    moviesListELem.innerHTML = moviesData.Search.map((movie) => moviesHTML(movie)).join("")
+
+    console.log(moviesData)
+}
+
+
+main()
+
+
+function moviesHTML(movie) {
+    return `
+    <div class="movies">
+        <div class="movie">
+            <figure class="movie__img--wrapper">
+                <img src="${movie.Poster}" class="movie__img" alt="">
+            </figure>
+            <div class="movie__title">
+            ${movie.Title}
+            </div>
+        </div>
+    </div>
+    `
+}
+
+
+
+
+/* <div class="movies__container">
+    <div class="movies-list">
+        <div class="movie">
+            <img src="${movie.Poster}" class="movie__img" alt="">
+            <h3 class="movie__title">${movie.Title}</h3>
+        </div>
+    </div
+</div> */
