f654d470804b33017d586c7a6b36df73d60cb15c
nathaniaria <nathan.iaria7@gmail.com>
2022-12-23 14:29:39 +1100

    correctly pulling data from api and displaying, now beginning on sort function

--- index.js
+++ index.js
@@ -2,29 +2,40 @@
 // 
 
 const moviesListELem = document.querySelector(".movies__row")
+const moviesSearchElem = document.querySelector(".searchResult")
+const id = localStorage.getItem(".searchResult")
+
+
 
 async function onSearchChange(event) {
     const id = event.target.value
     renderMovies(id)
+    moviesSearchElem.innerHTML = id
 }
+
+const moviesWrapper = document.querySelector('.movieSpin');
+
+
+function renderMovies(filter) {
+  if (filter === 'NEWEST_TO_OLDEST') {
+    movies.sort((a, b) => a.Year - b.Year);
+  } else if (filter === 'OLDEST_TO_NEWEST') {
+    movies.sort((a, b) => b.Year - a.Year);
+  }
+
+}
+
+function filterMovies(event) {
+  renderMovies(event.target.value);
+}
+
 
 async function renderMovies(id) {
     const movies = await fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=f97dee03&s=${id}`)
     const moviesData = await movies.json()
     moviesListELem.innerHTML = moviesData.Search.map((movie) => moviesHTML(movie)).join("")
-
-}
-
-
-
-
-async function main(id) {
-    const movies = await fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=f97dee03&s=${id}`)
-    const moviesData = await movies.json()
-    moviesListELem.innerHTML = moviesData.Search.map((movie) => moviesHTML(movie)).join("")
     console.log(moviesData)
 }
-
 
 
 
@@ -42,7 +53,7 @@ function moviesHTML(movie) {
     </div>
     `
 }
+renderMovies(id)
 
-renderMovies()
-main()
+
 
