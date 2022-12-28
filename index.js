7d60c8b6839b7ebbd3c7792a0b78dbfcb6046545
nathaniaria <nathan.iaria7@gmail.com>
2022-12-28 12:54:00 +1100

    attempt at loading state

--- index.js
+++ index.js
@@ -1,60 +1,49 @@
 // api https://www.omdbapi.com/?i=tt3896198&apikey=f97dee03
 // 
 
-const moviesListELem = document.querySelector(".movies__row")
-const moviesSearchElem = document.querySelector(".searchResult")
-const id = localStorage.getItem("id")
-let movies;
+const moviesListELem = document.querySelector(".movies__row");
+const moviesSearchElem = document.querySelector(".searchResult");
+const id = localStorage.getItem("id");
+
 let movie;
 let filtermovies;
 
-
 async function onSearchChange(event) {
-    const id = event.target.value
-    renderMovies(id)
-    moviesSearchElem.innerHTML = id
+  const id = event.target.value;
+  renderMovies(id);
+  moviesSearchElem.innerHTML = id;
 }
 
+const moviesWrapper = document.querySelector(".movies");
 
 function renderMovies(filter) {
-  const moviesWrapper = document.querySelector('.movies')
+  moviesWrapper.classList.add("movies__loading");
 
-  
-  moviesWrapper.classList += ' movies__loading'
-
-  setTimeout((
-    moviesWrapper.classList.remove('movies__loading')
-  ), 1000)
-
-  if (filter === 'NEWEST_TO_OLDEST') {
-    movies.sort((a, b) => (a.Year || a.year) - (b.Year || b.Year))
-    moviesListELem.innerHTML = moviesData.Search.map((movie) => moviesHTML(movie)).join("")
-  } 
-  else if (filter === 'OLDEST_TO_NEWEST') {
-    movies.sort((a, b) => (b.Year || b.year) - (a.Year || a.Year))
-    moviesListELem.innerHTML = moviesData.Search.map((movie) => moviesHTML(movie)).join("")
+  if (filter === "NEWEST_TO_OLDEST") {
+    movies.sort((a, b) => (a.Year || a.year) - (b.Year || b.Year));
+    moviesListELem.innerHTML = movies.map((movie) => moviesHTML(movie)).join("");
+  } else if (filter === "OLDEST_TO_NEWEST") {
+    movies.sort((a, b) => (b.Year || b.year) - (a.Year || a.Year));
+    moviesListELem.innerHTML = movies.map((movie) => moviesHTML(movie)).join("");
   }
 
-}
-
-function filterMovies(event) {
-  renderMovies(event.target.value);
-}
-
-
-async function renderMovies(id) {
-    const movies = await fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=f97dee03&s=${id}`)
-    const moviesData = await movies.json()
-    if (moviesData && Array.isArray(moviesData.Search)) {
-      moviesListELem.innerHTML = moviesData.Search.map((movie) => moviesHTML(movie)).join("")
-    console.log(moviesData)
+  if (movies && movies.length > 0) {
+    moviesWrapper.classList.remove("movies__loading");
 }}
 
-
-// moviesListELem.innerHTML = moviesData.Search.map((movie) => moviesHTML(movie)).join("")
+async function renderMovies(id) {
+  const moviesResponse = await fetch(
+    `https://www.omdbapi.com/?i=tt3896198&apikey=f97dee03&s=${id}`);
+  const moviesData = await moviesResponse.json();
+  if (moviesData && Array.isArray(moviesData.Search)) {
+    movies = moviesData.Search;
+    moviesListELem.innerHTML = movies.map((movie) => moviesHTML(movie)).join("");
+    console.log(moviesData);
+  }
+}
 
 function moviesHTML(movie) {
-    return `
+  return `
         <div class="movie">
             <figure class="movie__img--wrapper">
                 <img src="${movie.Poster}" class="movie__img" alt="">
@@ -64,8 +53,7 @@ function moviesHTML(movie) {
             </div>
         </div>
     </div>
-    `
+    `;
 }
-renderMovies(id)
 
-
+renderMovies(id);
