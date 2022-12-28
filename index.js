c4288b5450f9139d9f90fe24111599ecfef6b55b
nathaniaria <nathan.iaria7@gmail.com>
2022-12-28 18:16:10 +1100

    formatted

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // api https://www.omdbapi.com/?i=tt3896198&apikey=f97dee03
-// 
+//
 
 const moviesListELem = document.querySelector(".movies__row");
 const moviesSearchElem = document.querySelector(".searchResult");
@@ -17,38 +17,39 @@ async function onSearchChange(event) {
 const moviesWrapper = document.querySelector(".movies");
 
 function renderMovies(filter) {
-
   moviesWrapper.classList.add("movies__loading");
-
 
   if (filter === "NEWEST_TO_OLDEST") {
     movies.sort((a, b) => (b.Year || b.year) - (a.Year || a.year));
-    moviesListELem.innerHTML = movies.map((movie) => moviesHTML(movie)).join("");
+    moviesListELem.innerHTML = movies
+      .map((movie) => moviesHTML(movie))
+      .join("");
   } else if (filter === "OLDEST_TO_NEWEST") {
     movies.sort((a, b) => (a.Year || a.year) - (b.Year || b.year));
-    moviesListELem.innerHTML = movies.map((movie) => moviesHTML(movie)).join("");
+    moviesListELem.innerHTML = movies
+      .map((movie) => moviesHTML(movie))
+      .join("");
   }
-
-
-  
 }
 
 function filterMovies(event) {
   renderMovies(event.target.value);
 }
 
-
 async function renderMovies(id) {
-  const moviesResponse = await fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=f97dee03&s=${id}`);
+  const moviesResponse = await fetch(
+    `https://www.omdbapi.com/?i=tt3896198&apikey=f97dee03&s=${id}`
+  );
   const moviesData = await moviesResponse.json();
   if (moviesData && Array.isArray(moviesData.Search)) {
     movies = moviesData.Search;
-    moviesListELem.innerHTML = movies.map((movie) => moviesHTML(movie)).join("");
+    moviesListELem.innerHTML = movies
+      .map((movie) => moviesHTML(movie))
+      .join("");
     console.log(moviesData);
 
-    moviesWrapper.classList.remove ('movies__loading')
+    moviesWrapper.classList.remove("movies__loading");
   }
-
 }
 
 function moviesHTML(movie) {
@@ -64,6 +65,5 @@ function moviesHTML(movie) {
     </div>
     `;
 }
-
 
 renderMovies(id);
